fdf4e1550572684138d4042c6be11cb28c418814
supercartoon <supercartoon@naver.com>
2018-06-29 21:28:42 +0900

    add getWidth, getHeight

--- Source/iOS/Device.swift
+++ Source/iOS/Device.swift
@@ -163,3 +163,22 @@ open class Device {
     }
     
 }
+
+extension Device {
+
+    static func getWidth(width: CGFloat) -> CGFloat {
+        let deviceWidth = UIScreen.main.bounds.width
+        if deviceWidth == 375 {
+            return width
+        }
+        return width * (deviceWidth / 375)
+    }
+
+    static func getHeight(height: CGFloat) -> CGFloat {
+        let deviceHeight = UIScreen.main.bounds.height
+        if deviceHeight == 667 {
+            return height
+        }
+        return height * (deviceHeight / 667)
+    }
+}
